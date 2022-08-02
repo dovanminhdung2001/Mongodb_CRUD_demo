8f2a95990fe7e725972140e74f039b9980d969a8
MinhDung <dovanminhdung2001@gmail.com>
2022-08-02 10:47:48 +0700

    .

--- mongo_notDone/src/main/java/com/example/mongo_notDone/MongoNotDoneApplication.java
+++ mongo_notDone/src/main/java/com/example/mongo_notDone/MongoNotDoneApplication.java
@@ -1,35 +1,14 @@
 package com.example.mongo_notDone;
 
-import model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import repository.UserRepository;
 
 @SpringBootApplication
-public class MongoNotDoneApplication implements CommandLineRunner {
-
-	private final UserRepository userRepository;
-
-	@Autowired
-	public MongoNotDoneApplication(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+public class MongoNotDoneApplication  {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoNotDoneApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		if(userRepository.findAll().isEmpty()){
-			userRepository.save(new User("u1", "pw1"));
-			userRepository.save(new User("u2", "pw2"));
-		}
-
-		userRepository.findAll().forEach(System.out::println);
 	}
 
 }
